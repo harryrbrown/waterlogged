b4869fd4fca28f3295e95b62185b73b511137f1d
Harry Brown <harry.brown26@gmail.com>
2024-08-10 17:35:00 -0400

    Add manual input button

--- app/src/main/java/com/example/waterlogged/tile/MainTileService.kt
+++ app/src/main/java/com/example/waterlogged/tile/MainTileService.kt
@@ -10,8 +10,9 @@ import androidx.wear.protolayout.ModifiersBuilders
 import androidx.wear.protolayout.ResourceBuilders
 import androidx.wear.protolayout.TimelineBuilders
 import androidx.wear.protolayout.material.Button
+import androidx.wear.protolayout.material.CircularProgressIndicator
+import androidx.wear.protolayout.material.layouts.EdgeContentLayout
 import androidx.wear.protolayout.material.layouts.MultiButtonLayout
-import androidx.wear.protolayout.material.layouts.PrimaryLayout
 import androidx.wear.tiles.RequestBuilders
 import androidx.wear.tiles.TileBuilders
 import com.example.waterlogged.R
@@ -38,6 +39,7 @@ class MainTileService : SuspendingTileService() {
             .addIdToImageMapping("glass", drawableResToImageResource(R.drawable.glass_cup_24px))
             .addIdToImageMapping("bottle", drawableResToImageResource(R.drawable.tile_preview))
             .addIdToImageMapping("large_bottle", drawableResToImageResource(R.drawable.water_bottle_large_24px))
+            .addIdToImageMapping("keyboard", drawableResToImageResource(R.drawable.keyboard_24px))
             .build()
     }
 
@@ -72,16 +74,28 @@ private fun tileLayout(context: Context): LayoutElementBuilders.LayoutElement {
     val glassButton = buttonLayout(context, emptyClickable, "glass")
     val bottleButton = buttonLayout(context, emptyClickable, "bottle")
     val largeBottleButton = buttonLayout(context, emptyClickable, "large_bottle")
+    val keyboardButton = buttonLayout(context, emptyClickable, "keyboard")
 
-    return PrimaryLayout.Builder(buildDeviceParameters(context.resources))
+    return EdgeContentLayout.Builder(buildDeviceParameters(context.resources))
         .setResponsiveContentInsetEnabled(true)
         .setContent(
             MultiButtonLayout.Builder()
                 .addButtonContent(glassButton)
                 .addButtonContent(bottleButton)
                 .addButtonContent(largeBottleButton)
+                .addButtonContent(keyboardButton)
                 .build()
-        ).build()
+        )
+        .setEdgeContent(
+            CircularProgressIndicator.Builder()
+                .setStartAngle(30.0f)
+                .setEndAngle(330.0f)
+                .setStrokeWidth(5.0f)
+                .setProgress(0.5f)
+                .build()
+        )
+        .setEdgeContentThickness(5.0f)
+        .build()
 }
 
 @Preview(
@@ -96,6 +110,7 @@ fun TilePreview() {
         addIdToImageMapping("glass", drawableResToImageResource(R.drawable.glass_cup_24px))
         addIdToImageMapping("bottle", drawableResToImageResource(R.drawable.water_bottle_24px))
         addIdToImageMapping("large_bottle", drawableResToImageResource(R.drawable.water_bottle_large_24px))
+        addIdToImageMapping("keyboard", drawableResToImageResource(R.drawable.keyboard_24px))
     }
 
     LayoutRootPreview(root = tileLayout(LocalContext.current), tileResourcesFn = previewResources )
