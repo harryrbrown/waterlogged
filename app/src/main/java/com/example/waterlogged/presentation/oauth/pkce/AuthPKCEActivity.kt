a842bf0f2cacb2d30736296052350f565b2e99d5
Harry Brown <harry.brown26@gmail.com>
2024-08-11 23:13:48 -0400

    Update signin button to use phone icon

--- app/src/main/java/com/example/waterlogged/presentation/oauth/pkce/AuthPKCEActivity.kt
+++ app/src/main/java/com/example/waterlogged/presentation/oauth/pkce/AuthPKCEActivity.kt
@@ -3,14 +3,19 @@ package com.example.waterlogged.presentation.oauth.pkce
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.wear.compose.material.Chip
+import androidx.wear.compose.material.ChipDefaults
+import androidx.wear.compose.material.Icon
 import androidx.wear.compose.material.ListHeader
 import androidx.wear.compose.material.Text
 import androidx.wear.compose.ui.tooling.preview.WearPreviewDevices
@@ -56,25 +61,34 @@ fun AuthenticateScreen(
             last = ScalingLazyColumnDefaults.ItemType.Text
         )
     )
-    ScreenScaffold(scrollState = columnState) {
+    ScreenScaffold(scrollState = columnState, modifier = Modifier.background(Color.Black)) {
         ScalingLazyColumn(columnState = columnState) {
             item {
                 ListHeader {
                     Text(
-                        stringResource(R.string.oauth_pkce),
+                        stringResource(R.string.waterlogged),
                         textAlign = TextAlign.Center
                     )
                 }
+            }
+            item {
+                Text(
+                    text = stringResource(R.string.sign_in_fitbit),
+                    textAlign = TextAlign.Center
+                )
             }
             item {
                 Chip(
                     onClick = { startAuthFlow() },
                     label = {
                         Text(
-                            text = stringResource(R.string.authenticate),
-                            modifier = Modifier.align(Alignment.CenterVertically)
+                            text = stringResource(R.string.sign_in_on_phone)
                         )
-                    }
+                    },
+                    icon = {
+                        Icon(painter = painterResource(id = R.drawable.send_to_mobile_24px), contentDescription = "Send to Phone")
+                    },
+                    colors = ChipDefaults.secondaryChipColors()
                 )
             }
             item { Text(stringResource(id = statusCode)) }
