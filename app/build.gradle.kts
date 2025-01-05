584bdde5240ad5f7301453befbc6273021a1820d
Harry Brown <harry.brown26@gmail.com>
2025-01-05 17:55:41 -0500

    Upgrade version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.hrb116.waterlogged"
         minSdk = 30
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.7.2"
+        versionCode = 11
+        versionName = "1.7.3"
         vectorDrawables {
             useSupportLibrary = true
         }
