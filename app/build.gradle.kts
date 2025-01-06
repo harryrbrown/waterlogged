6fa17cbf2e6a25f42b9937a501c7170db682ad9e
Harry Brown <harry.brown26@gmail.com>
2025-01-05 20:30:22 -0500

    Bump version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.hrb116.waterlogged"
         minSdk = 30
         targetSdk = 34
-        versionCode = 11
-        versionName = "1.7.3"
+        versionCode = 12
+        versionName = "1.7.4"
         vectorDrawables {
             useSupportLibrary = true
         }
